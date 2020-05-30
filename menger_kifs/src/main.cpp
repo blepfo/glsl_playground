669562f6093ae549f566e121cc0cdfcd116b316e
blepfo <adam.b.anderson.96@gmail.com>
2020-05-30 13:38:29 -0700

    Use TwoTriangleRenderer

--- menger_kifs/src/main.cpp
+++ menger_kifs/src/main.cpp
@@ -12,74 +12,33 @@
 // https://github.com/blepfo/opengl_utils
 #include "opengl_utils/include/Camera.h"
 #include "opengl_utils/include/Shader.h"
-#include "opengl_utils/include/SimpleRenderer.h"
+#include "opengl_utils/include/TwoTrianglesRenderer.h"
 #include "opengl_utils/include/texture.h"
 
 
-class MengerRenderer : public SimpleRenderer {
+class MengerRenderer : public TwoTrianglesRenderer {
     public: 
         // Need to declare overridden functions BEFORE constructor
         // to avoid vtable errors
-        void initScene();
         void processInputs();
         void createGui();
-        void renderObjects();
+        void setUniforms();
 
         MengerRenderer(
+            const char* fragmentShaderPath,
             int screenWidth, 
             int screenHeight, 
             const char* windowName,
             Camera* camera
-        ) : SimpleRenderer(screenWidth, screenHeight, windowName, true),
+        ) : TwoTrianglesRenderer(fragmentShaderPath, screenWidth, screenHeight, windowName, true),
             camera(camera) {}
 
     protected:
-        // TODO - SceneObject class
-        unsigned int vao;
-        unsigned int vbo;
-        unsigned int ebo;
-        Shader* shader;
         Camera* camera;
         float ao=0.8;
         float marchHitDist=0.01;
 };
 
-void MengerRenderer::initScene() {
-        // Vertex data          
-        float vertices[] = {
-            -1.0, -1.0, 0.0,
-            -1.0, 1.0, 0.0,
-            1.0, -1.0, 0.0,
-            1.0, 1.0, 0.0,
-        };
-        unsigned int indices[] = {
-            0, 1, 2,
-            1, 3, 2
-        };
-        unsigned int vao;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-        unsigned int vbo;
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*) 0);
-        glEnableVertexAttribArray(0);
-        unsigned int ebo;
-        glGenBuffers(1, &ebo);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-        // Store for access in renderObjects()
-        this->vao = vao;
-        this->vbo = vbo;
-        this->ebo = ebo;
-    
-        // Init shader    
-        this->shader = new Shader(
-            "./src/twotriangle.vs", 
-            "./src/test.fs"
-        );
-}
 
 void MengerRenderer::createGui() {
     // Dear ImGui
@@ -93,69 +52,27 @@ void MengerRenderer::createGui() {
     ImGui::End();
 }
 
-void MengerRenderer::renderObjects() {
-    // Render TwoTriangles
-    glBindVertexArray(this->vao);
-    this->shader->activate();
+
+void MengerRenderer::setUniforms() {
     this->shader->setVec2("iResolution", glm::vec2(this->screenWidth, this->screenHeight));
     // Camera
     this->shader->setVec3("eye", this->camera->getOrigin());
     this->shader->setVec3("forward", this->camera->getForward());
     this->shader->setVec3("up", this->camera->getUp());
     this->shader->setVec3("right", this->camera->getRight());
-
     // Shader params
     this->shader->setFloat("GLOBAL_AO", this->ao);
     this->shader->setFloat("MARCH_HIT_DIST", this->marchHitDist);
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
+
 void MengerRenderer::processInputs() {
-    GLFWwindow* window = this->_window;
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-        std::cout << "ESC KEY -> CLOSE WINDOW" << std::endl;
-    }
-    // Walk input
-    Camera* camera = this->camera;
-    float deltaTime = this->deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        camera->translate(CameraDirection::FORWARD, true, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        camera->translate(CameraDirection::RIGHT, false, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        camera->translate(CameraDirection::FORWARD, false, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-        camera->translate(CameraDirection::RIGHT, true, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
-        camera->translate(CameraDirection::UP, false, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
-        camera->translate(CameraDirection::UP, true, deltaTime);
-    }
-    // Rotation
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-        camera->updateRotation(1.0f, 0.0f);
-    }
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        camera->updateRotation(-1.0f, 0.0f);
-    }
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        camera->updateRotation(0.0f, 1.0f);
-    }
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        camera->updateRotation(0.0f, -1.0f);
-    }
+    Camera::standardWalkProcessing(this->camera, this->_window, this->deltaTime);
 }
 
 int main() {
     Camera camera(glm::vec3(0.0f, 0.0f, 0.0f), 0.0f, 0.0f, 0.1f, 0.025f);
-    MengerRenderer m = MengerRenderer(400, 400, "window", &camera);
+    MengerRenderer m = MengerRenderer("./src/test.fs", 400, 400, "window", &camera);
     std::cout << "CALL RUN" << std::endl;
     return m.run();
 }
